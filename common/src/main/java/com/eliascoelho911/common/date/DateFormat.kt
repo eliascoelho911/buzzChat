a363eef8c99705ed474e9c9298aaa39856669122
elias <elias_coelho911@hotmail.com>
2023-08-30 22:52:16 -0300

    Create data formatters

--- common/src/main/java/com/eliascoelho911/common/date/DateFormat.kt
+++ common/src/main/java/com/eliascoelho911/common/date/DateFormat.kt
@@ -1,14 +1,23 @@
 package com.eliascoelho911.common.date
 
 import org.joda.time.LocalDateTime
+import org.joda.time.format.DateTimeFormat
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-fun LocalDateTime.formatToHours(onlyToday: Boolean = true): String {
-    return if (onlyToday && isToday()) toString("HH:mm")
-    else formatToSimpleDate()
+fun LocalDateTime.simpleFormat(locale: Locale): String {
+    return when {
+        isToday() -> formatToHours(locale)
+        isTomorrow() || isYesterday() -> formatToDayOfWeek(locale)
+        else -> formatToDate(locale)
+    }
 }
 
-fun LocalDateTime.formatToSimpleDate(): String {
-    return toString("dd/MM/yyyy")
-}
+fun LocalDateTime.formatToDayOfWeek(locale: Locale): String =
+    SimpleDateFormat("EEEE", locale).format(toDate())
 
-private fun LocalDateTime.isToday() = dayOfMonth == LocalDateTime.now().dayOfMonth
+fun LocalDateTime.formatToHours(locale: Locale): String =
+    toString(DateTimeFormat.forStyle("-S").withLocale(locale))
+
+fun LocalDateTime.formatToDate(locale: Locale): String =
+    toString(DateTimeFormat.forStyle("S-").withLocale(locale))
