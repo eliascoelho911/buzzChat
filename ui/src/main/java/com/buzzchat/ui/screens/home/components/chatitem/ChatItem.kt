8f5ad27ec4b61206bcfd13635872cd18d5138a67
elias <elias_coelho911@hotmail.com>
2023-09-03 16:53:02 -0300

    ChatItemState

--- ui/src/main/java/com/buzzchat/ui/screens/home/components/chatitem/ChatItem.kt
+++ ui/src/main/java/com/buzzchat/ui/screens/home/components/chatitem/ChatItem.kt
@@ -1,23 +1,18 @@
-package com.buzzchat.ui.screens.home.chat
+package com.buzzchat.ui.screens.home.components.chatitem
 
-import android.content.res.Configuration
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Badge
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
@@ -25,25 +20,21 @@ import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
 import com.buzzchat.ui.R
 import com.buzzchat.ui.components.common.ProfilePlaceholder
-import com.buzzchat.ui.previews.LightDarkPreview
-import com.buzzchat.ui.screens.home.chat.model.Chat
-import com.buzzchat.ui.theme.BuzzChatTheme
+import com.eliascoelho911.common.date.localDateTime
 import com.eliascoelho911.common.date.simpleFormat
-import org.joda.time.LocalDateTime
-import java.util.Locale
 
 @Composable
 fun ChatItem(
-    chat: Chat,
+    state: ChatItemState,
     modifier: Modifier = Modifier,
-    onClick: (Chat) -> Unit = {},
+    onClick: () -> Unit = {},
 ) {
-    with(chat) {
-        Surface(onClick = { onClick(chat) }, modifier = modifier) {
+    with(state) {
+        Surface(onClick = onClick, modifier = modifier) {
             ConstraintLayout(modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)) {
                 val (pictureRef, personNameRef, lastMessageRef, lastMessageTimeRef, unreadBadgeRef) = createRefs()
 
-                Picture(url = personPictureUrl, modifier = Modifier.constrainAs(pictureRef) {
+                Picture(url = avatarUrl, modifier = Modifier.constrainAs(pictureRef) {
                     start.linkTo(parent.start)
                     top.linkTo(parent.top)
                     bottom.linkTo(parent.bottom)
@@ -53,9 +44,15 @@ fun ChatItem(
                     text = personName,
                     style = MaterialTheme.typography.titleSmall,
                     modifier = Modifier.constrainAs(personNameRef) {
-                        start.linkTo(pictureRef.end, margin = HorizontalSpacing)
+                        start.linkTo(
+                            pictureRef.end,
+                            margin = HorizontalSpacing
+                        )
                         top.linkTo(pictureRef.top)
-                        end.linkTo(lastMessageTimeRef.start, margin = HorizontalSpacing)
+                        end.linkTo(
+                            lastMessageTimeRef.start,
+                            margin = HorizontalSpacing
+                        )
                         width = Dimension.fillToConstraints
                     }
                 )
@@ -64,7 +61,10 @@ fun ChatItem(
                     chat = this@with,
                     modifier = Modifier.Companion.constrainAs(lastMessageRef) {
                         start.linkTo(personNameRef.start)
-                        top.linkTo(personNameRef.bottom, margin = VerticalSpacing)
+                        top.linkTo(
+                            personNameRef.bottom,
+                            margin = VerticalSpacing
+                        )
                         end.linkTo(
                             if (showUnreadBadge) unreadBadgeRef.start else parent.end,
                             margin = HorizontalSpacing
@@ -73,7 +73,7 @@ fun ChatItem(
                     }
                 )
                 Text(
-                    text = lastMessageTime.simpleFormat(LocalConfiguration.current.locales[0]),
+                    text = localDateTime(lastMessageMillis).simpleFormat(LocalConfiguration.current.locales[0]),
                     style = MaterialTheme.typography.bodyMedium,
                     modifier = Modifier.constrainAs(lastMessageTimeRef) {
                         end.linkTo(parent.end)
@@ -95,7 +95,7 @@ fun ChatItem(
 
 @Composable
 private fun Message(
-    chat: Chat,
+    chat: ChatItemState,
     modifier: Modifier = Modifier,
 ) {
     val text: String
@@ -148,41 +148,3 @@ private fun Picture(url: String, modifier: Modifier = Modifier) {
 private val ImageSize = 48.dp
 private val VerticalSpacing = 8.dp
 private val HorizontalSpacing = 16.dp
-
-@LightDarkPreview
-@Composable
-private fun ChatItemPreview(@PreviewParameter(SampleChatProvider::class) chat: Chat) {
-    val configuration = Configuration().apply {
-        setLocale(Locale("pt", "BR"))
-    }
-    CompositionLocalProvider(LocalConfiguration provides configuration) {
-        BuzzChatTheme {
-            ChatItem(
-                modifier = Modifier.width(300.dp),
-                chat = chat
-            )
-        }
-    }
-}
-
-private class SampleChatProvider : PreviewParameterProvider<Chat> {
-    override val values: Sequence<Chat>
-        get() = sequenceOf(
-            Chat(
-                personPictureUrl = "",
-                personName = "Elias Coelho",
-                lastMessage = "Olá, tudo bem? Como está a sua família? Estou com saudades de vocês.",
-                isTyping = false,
-                lastMessageTime = LocalDateTime.now().minusDays(2),
-                unreadMessages = 2
-            ),
-            Chat(
-                personPictureUrl = "",
-                personName = "Letícia Lucena",
-                lastMessage = "Você viu o que aconteceu? Estou muito preocupada!",
-                isTyping = true,
-                lastMessageTime = LocalDateTime.now(),
-                unreadMessages = 0
-            )
-        )
-}
---
+++ ui/src/main/java/com/buzzchat/ui/screens/home/components/chatitem/ChatItemState.kt
@@ -0,0 +1,40 @@
+package com.buzzchat.ui.screens.home.components.chatitem
+
+import androidx.compose.runtime.Immutable
+import com.buzzchat.ui.stubs.chat.model.Chat
+import com.buzzchat.ui.stubs.chat.model.PrivateChat
+
+@Immutable
+data class ChatItemState(
+    val id: String,
+    val avatarUrl: String,
+    val personName: String,
+    val lastMessage: String,
+    val lastMessageMillis: Long,
+    val unreadMessages: Int,
+    val isTyping: Boolean,
+) {
+    val showUnreadBadge: Boolean
+        get() = unreadMessages > 0
+}
+
+fun List<Chat>.toState(): List<ChatItemState> =
+    mapNotNull { it.toState() }
+
+fun Chat.toState(): ChatItemState? =
+    when (this) {
+        is PrivateChat -> {
+            messages.lastOrNull()?.let { lastMessage ->
+                ChatItemState(
+                    id = id,
+                    avatarUrl = otherUser.avatarUrl,
+                    personName = otherUser.name,
+                    lastMessage = lastMessage.content,
+                    lastMessageMillis = lastMessage.createAt,
+                    unreadMessages = messages.filter { it.sender.id != loggedUser.id }
+                        .count { !it.isRead },
+                    isTyping = otherUserIsTyping,
+                )
+            }
+        }
+    }
