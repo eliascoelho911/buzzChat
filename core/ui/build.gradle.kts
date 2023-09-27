caeb2e3b2a772d0bffcaa674f2777192f03fe714
elias <elias_coelho911@hotmail.com>
2023-09-26 22:52:58 -0300

    resolve cyclic dependency

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -22,7 +22,6 @@ dependencies {
 
     debugApi(libs.androidx.compose.ui.tooling)
 
-    implementation(project(":core:designsystem"))
     implementation(project(":core:domain"))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.core.ktx)
