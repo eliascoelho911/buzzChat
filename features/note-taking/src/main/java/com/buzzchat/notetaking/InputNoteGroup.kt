64936a721e8d13b0822f13bad83ee95a9d3f83a2
Elias Coelho <elias_coelho911@hotmail.com>
2023-10-15 22:21:04 -0300

    commit

--- features/note-taking/src/main/java/com/buzzchat/notetaking/InputNoteGroup.kt
+++ features/note-taking/src/main/java/com/buzzchat/notetaking/InputNoteGroup.kt
@@ -1,4 +1,4 @@
-package com.buzzchat.notetaking.inputnote
+package com.buzzchat.notetaking
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
--- features/note-taking/src/main/java/com/buzzchat/notetaking/NoteTakingScreen.kt
+++ features/note-taking/src/main/java/com/buzzchat/notetaking/NoteTakingScreen.kt
@@ -1,12 +1,19 @@
 package com.buzzchat.notetaking
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
 import com.buzzchat.core.designsystem.BuzzChatLogo
 import com.buzzchat.core.designsystem.theme.BuzzChatTheme
 import com.buzzchat.core.ui.DevicePreviews
+import com.buzzchat.core.ui.PhonePreview
 
 @Composable
 internal fun NoteTakingScreen() {
@@ -34,11 +41,29 @@ private fun NoteTakingContent(innerPadding: PaddingValues) {
 }
 
 @Composable
-private fun InputNoteBottomSheet() {
+private fun InputNoteBottomSheet(viewModel: NoteTakingViewModel) {
+    Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+        Column {
+            InputNoteGroup(
+                selectedGroup = viewModel.selectedGroup,
+                onSelectedGroupChange = viewModel::onSelectedGroupChange,
+                onCreateNewGroup = viewModel::onCreateNewGroup,
+                groupOptions = viewModel.groupOptions,
+                onSearchGroups = viewModel::onSearchGroups,
+                showGroupOptions = viewModel.,
+                onShowGroupOptionsChange = ,
+                isEditing = ,
+                onIsEditingChange = 
+            )
+        }
+    }
+}
+
+internal class NoteTakingViewModel: ViewModel() {
 
 }
 
-@DevicePreviews
+@PhonePreview
 @Composable
 private fun NoteTakingScreenPreview() {
     BuzzChatTheme {
